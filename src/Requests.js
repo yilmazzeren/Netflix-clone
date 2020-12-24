e24730355327ff0158372a4c06b1d50fc8a2a9d0
unknown <yilmazzeren93@gmail.com>
2020-12-24 19:36:21 +0300

    Tmdb altyapısı oluşturuldu,posterler çekildi

---
+++ src/Requests.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-dupe-keys */
+/* eslint-disable no-unused-vars */
+const API_KEY = "1f8df082547eba8a242183d2c9968dea";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
